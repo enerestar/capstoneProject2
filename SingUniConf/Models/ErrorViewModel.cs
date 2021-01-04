c9e43924901778f52205d927dabc195ec54361e2
Serene Lim <enerestar@gmail.com>
2021-01-04 17:17:04 +0800

    Initial commit
    Signed-off-by: Serene Lim <enerestar@gmail.com>

---
+++ SingUniConf/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SingUniConf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
